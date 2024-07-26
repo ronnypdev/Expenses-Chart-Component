8be0996f05630421e3e0c9655bd8be18bf6cc5d2
Ronny Perez <ronnypdev@gmail.com>
2024-07-26 17:32:55 -0400

    implemented starter files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1 className="text-3xl font-bold underline">Keep Track of your finances</h1>
+
+      </main>
+    </>
+  )
+}
+
+export default App
