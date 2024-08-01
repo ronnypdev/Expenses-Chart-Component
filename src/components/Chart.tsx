cfc3ac96fb37eaafe08253296bdbe83bd9e197a2
Ronny Perez <ronnypdev@gmail.com>
2024-08-01 11:56:23 -0400

    Implemented chart component css complete

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -16,47 +16,47 @@ export default function Chart() {
           />
         </header>
         <div className="bg-paleOrange p-5 chart-container rounded-l-2xl rounded-r-2xl">
-          <h4>Spending - Last 7 days</h4>
+          <h4 className="font-DMsans text-[32px] text-darkBrown leading-normal font-bold mb-10">Spending - Last 7 days</h4>
 
-          <div className="chart">
-            <div>
-              <div></div>
-              <p>mon</p>
+          <div className="chart flex justify-between items-baseline flex-shrink-0 border-b-2 border-cream pb-5">
+            <div className="flex flex-col justify-between items-center">
+              <div className="w-[50.365px] h-[50px] bg-softRed hover:opacity-55 flex-shrink-0 rounded-[5px] cursor-pointer"></div>
+              <p className="font-DMsans text-[15px] text-darkBrown leading-normal font-normal lowercase mt-2">mon</p>
             </div>
-            <div>
-              <div></div>
-              <p>tue</p>
+            <div className="flex flex-col justify-between items-center">
+              <div className="w-[50.365px] h-[100px] bg-softRed hover:opacity-55 flex-shrink-0 rounded-[5px] cursor-pointer"></div>
+              <p className="font-DMsans text-[15px] text-darkBrown leading-normal font-normal lowercase mt-2">tue</p>
             </div>
-            <div>
-              <div></div>
-              <p>wed</p>
+            <div className="flex flex-col justify-between items-center">
+              <div className="w-[50.365px] h-[150px] bg-cyan hover:opacity-55 flex-shrink-0 rounded-[5px] cursor-pointer"></div>
+              <p className="font-DMsans text-[15px] text-darkBrown leading-normal font-normal lowercase mt-2">wed</p>
             </div>
-            <div>
-              <div></div>
-              <p>thu</p>
+            <div className="flex flex-col justify-between items-center">
+              <div className="w-[50.365px] h-[89px] bg-softRed hover:opacity-55 flex-shrink-0 rounded-[5px] cursor-pointer"></div>
+              <p className="font-DMsans text-[15px] text-darkBrown leading-normal font-normal lowercase mt-2">thu</p>
             </div>
-            <div>
-              <div></div>
-              <p>fri</p>
+            <div className="flex flex-col justify-between items-center">
+              <div className="w-[50.365px] h-[67px] bg-softRed hover:opacity-55 flex-shrink-0 rounded-[5px] cursor-pointer"></div>
+              <p className="font-DMsans text-[15px] text-darkBrown leading-normal font-normal lowercase mt-2">fri</p>
             </div>
-            <div>
-              <div></div>
-              <p>sat</p>
+            <div className="flex flex-col justify-between items-center">
+              <div className="w-[50.365px] h-[124px] bg-softRed hover:opacity-55 flex-shrink-0 rounded-[5px] cursor-pointer"></div>
+              <p className="font-DMsans text-[15px] text-darkBrown leading-normal font-normal lowercase mt-2">sat</p>
             </div>
-            <div>
-              <div></div>
-              <p>sun</p>
+            <div className="flex flex-col justify-between items-center">
+              <div className="w-[50.365px] h-[73px] bg-softRed hover:opacity-55 flex-shrink-0 rounded-[5px] cursor-pointer"></div>
+              <p className="font-DMsans text-[15px] text-darkBrown leading-normal font-normal lowercase mt-2">sun</p>
             </div>
           </div>{/*End chart inner container*/}
 
-          <div>
+          <div className="mt-8 flex justify-between items-center">
             <div>
-              <p>Total this month</p>
-              <h2>$478.33</h2>
+              <p className="font-DMsans text-lg text-mediumBrown leading-normal font-normal">Total this month</p>
+              <h2 className="font-DMsans text-[48px] text-darkBrown leading-normal font-bold">$478.33</h2>
             </div>
             <div>
-              <p>+2.4%</p>
-              <h2>from last month</h2>
+              <p className="font-DMsans text-lg text-darkBrown leading-normal font-bold">+2.4%</p>
+              <h2 className="font-DMsans text-lg text-mediumBrown leading-normal font-normal">from last month</h2>
             </div>
           </div>
         </div> {/*End chart container*/}
